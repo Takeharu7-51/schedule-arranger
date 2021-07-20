896471acb96c078b9e7c9807f3d3f9e29552ccbc
takeharu7-51 <55825376+Takeharu7-51@users.noreply.github.com>
2021-07-20 14:25:34 +0900

    Add files via upload

---
+++ models/sequelize-loader.js
@@ -0,0 +1,16 @@
+'use strict';
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/schedule_arranger',
+  {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
+module.exports = {
+  database: sequelize,
+  Sequelize: Sequelize
+};
